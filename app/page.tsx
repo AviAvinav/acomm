12a5b8b5ce90bf460a1ca754c55b39dd222aa9c1
Avi Avinav <aviavinav2004@gmail.com>
2023-07-28 23:42:49 +0530

    add an hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import Container from "~/components/container";
 
 export default function Home() {
-  return <Container>Stuff</Container>;
+  return (
+    <Container>
+      <div className="relative w-full">
+        {/* eslint-disable-next-line @next/next/no-img-element*/}
+        <img
+          className="w-full h-sm rounded object-cover"
+          src="/hero.jpg"
+          alt=""
+        />
+        <div className="w-full h-sm absolute top-0 left-0 right-0 rounded z-10 bg-gradient-to-b from-transparent @dark:to-dark" />
+      </div>
+    </Container>
+  );
 }
